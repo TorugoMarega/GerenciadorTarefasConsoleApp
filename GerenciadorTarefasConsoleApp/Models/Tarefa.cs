0573631cd91da14ea4f443a4091ea3f0a62dc0e2
Victor Hugo Marega <victorhugomarega@hotmail.com>
2025-04-15 22:33:03 -0300

    Criação dos métodos principais de cadastro e llistagem de tarefas. Mock para testar a exibição da lista de tarefas. Criação do menu principal da aplicação

--- GerenciadorTarefasConsoleApp/Models/Tarefa.cs
+++ GerenciadorTarefasConsoleApp/Models/Tarefa.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GerenciadorTarefasConsoleApp.Enum;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,21 @@ using System.Threading.Tasks;
 
 namespace GerenciadorTarefasConsoleApp.Models
 {
-    class Tarefa
+    public class Tarefa
     {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
 
+        public string Descricao { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataConclusao { get; set; }
+        public StatusEnum Status { get; set; }
+        public Tarefa(string titulo, string descricao)
+        {   
+            Titulo = titulo;
+            Descricao = descricao;
+            DataCriacao = DateTime.Now;
+            Status = StatusEnum.PENDENTE;
+        }
     }
 }
